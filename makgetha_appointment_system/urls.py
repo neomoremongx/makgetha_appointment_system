d7110de47763abfd8afb94d0a3836d9dbd78a6e3
neomoremongx <neomoremongx@gmail.com>
2026-03-11 22:08:21 +0200

    Update urls.py

--- makgetha_appointment_system/urls.py
+++ makgetha_appointment_system/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.get_home, name='home'),
+    path('home', views.get_home, name='home'),
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('appointment/create/', views.create_appointment, name='create_appointment'),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('appointment/<int:id>/delete/', views.delete_appointment, name='delete_appointment'),
     path('appointment/<int:id>/', views.get_appointment_detail, name='appointment_detail'),
     path('search/', views.search_appointment, name='search_appointment'),
+
 ]
