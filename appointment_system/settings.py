015b62b263b00db071b990b99390990526dc03b6
neomoremongx <neomoremongx@gmail.com>
2026-03-14 01:14:27 +0200

    Update settings.py

--- appointment_system/settings.py
+++ appointment_system/settings.py
@@ -18,6 +18,9 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#load .env variables
+load_dotenv()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
